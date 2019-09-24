9b89545ef514510197ab69d8a58f5de9f90a2e73
Arat Mizhit-Dorzhu <mizhitd@mail.com>
2019-09-24 14:12:22 +0800

    Список дел

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,45 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <main id="todolist">
+      <h1>
+        СПИСОК ДЕЛ:
+      </h1>
+      <TodoList v-bind:todos="todos" 
+      @remove-todo="removeTodo" v-if="todos.length"
+        />
+      <p class="emptylist" v-else>Список пуст</p>
+      <AddTodo @add-todo="addTodo"/>
+    </main>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import TodoList from "@/components/TodoList"
+import AddTodo from "@/components/AddTodo"
 export default {
   name: 'app',
+  data() {
+    return {
+      todos: [
+        {id: 1, title:"Прочитать книгу 'Алхимик' ", completed: false},
+        {id: 2, title:"Создать чат на NodeJS", completed: false},
+        {id: 3, title:"Выиграть хакатон", completed: false}
+      ]
+    }
+  },
+  mounted() {
+
+  },
+  methods: {
+    removeTodo(id) {
+      this.todos = this.todos.filter(t=> t.id !== id)
+    },
+    addTodo(todo) {
+      this.todos.push(todo)
+    }
+  },
   components: {
-    HelloWorld
+   TodoList,AddTodo
   }
 }
 </script>
@@ -24,5 +52,41 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+* {
+	margin:0;
+	padding:0;
+	box-sizing:border-box;
+}
+html, body {
+	background:#7CB6FC;
+	font-size:1.1rem;
+	font-family:'Helvetica', sans-serif;
+	height:100%;
+}
+#todolist {
+	margin:4rem auto;
+	padding:2rem 3rem 3rem;
+	max-width:500px;
+	background:#617FA0;
+	color:#FFF;
+  border-radius: 10px;
+	-webkit-box-shadow: 10px 10px 50px 0px rgba(0,0,0,0.75);
+  -moz-box-shadow: 10px 10px 50px 0px rgba(0,0,0,0.75);
+  box-shadow: 10px 10px 50px 0px rgba(0,0,0,0.75);
+}
+#todolist h1 {
+    font-weight:normal;
+    font-size:2.6rem;
+    letter-spacing:0.05em;
+    border-bottom:1px solid rgba(255,255,255,.3); 
+    margin-bottom: 1em;
+}
+#todolist .emptylist {
+	margin-top:2.6rem;
+	text-align:center;
+	letter-spacing:.05em;
+	font-style:italic;
+	opacity:0.8;
 }
 </style>
